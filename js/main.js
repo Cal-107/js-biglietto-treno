e29a4400619e9cd67767438bd3155d1efec019f9
“Cal-107” <kaly___7@hotmail.it>
2021-10-21 16:44:12 +0200

    add two decimal number block

--- js/main.js
+++ js/main.js
@@ -2,33 +2,24 @@
 
 // Chiedere l'età del passeggero //
 const age = parseInt (prompt('Prego inserisca la sua età'));
-console.log(age);
 
 // Chiedere quanti chilometri si vuole percorrere //
 const km = parseInt (prompt('Prego inserisca quanti chilometri deve percorrere'));
-console.log(km);
 
 //Prezzo di base del biglietto
 let priceTicket = 0.21;
-console.log(priceTicket)
-
-
-
 
 //Calcolo prezzo biglietto
-// - Prezzo in base ai km (0.21 € al km)
-// - Sconto sul biglietto del 20% ai minorenni
-// - Sconto sul biglietto del 40% per gli over 65
 let totTicket = priceTicket * km;
-console.log('Prezzo totale del ticket', totTicket,'€')
 
 if (age < 18) {
     totTicket = totTicket - (totTicket * 20 / 100);
-
+    
 } else if (age > 65) {
     totTicket = totTicket - (totTicket * 40 / 100);
 }
 
+let totFinTicket = totTicket.toFixed(2);
 
 // Output a schermo
-document.getElementById('final-price').innerHTML = totTicket;
+document.getElementById('final-price').innerHTML = `Il prezzo del suo biglietto è: € ${totFinTicket}`
