0fa1d466e3c013690aff30af695b2d274951da88
“Cal-107” <kaly___7@hotmail.it>
2021-10-21 13:27:00 +0200

    add setup

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Biglietto del treno //
+
+console.log('Hello world')
+
+// Chiedere l'età del passeggero //
+
+
+// Chiedere quanti chilometri si vuole percorrere //
+
+
+//Calcolo prezzo biglietto
+// - Prezzo in base ai km (0.21 € al km)
+// - Sconto sul biglietto del 20% ai minorenni
+// - Sconto sul biglietto del 40% per gli over 65
+
+
+// Output a schermo
