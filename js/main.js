a74126df7c2e3b0b956ffdb5bf2c3a85a34d5b83
“Cal-107” <kaly___7@hotmail.it>
2021-10-21 14:47:45 +0200

    add tickt's price

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 // Biglietto del treno //
 
-console.log('Hello world')
-
 // Chiedere l'età del passeggero //
-
+const age = parseInt (prompt('Prego inserisca la sua età'));
+console.log(age);
 
 // Chiedere quanti chilometri si vuole percorrere //
+const km = parseInt (prompt('Prego inserisca quanti chilometri deve percorrere'));
+console.log(km);
+
+//Prezzo di base del biglietto
+let priceTicket = 0.21;
+console.log(priceTicket)
+
+
 
 
 //Calcolo prezzo biglietto
 // - Prezzo in base ai km (0.21 € al km)
 // - Sconto sul biglietto del 20% ai minorenni
 // - Sconto sul biglietto del 40% per gli over 65
+let totTicket = priceTicket * km;
+console.log('Prezzo totale del ticket', totTicket,'€')
+
+if (age) {
+    
+}
 
 
 // Output a schermo
